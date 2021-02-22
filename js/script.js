2e4d6131c980d48043ccf133d2bc8161ca196f3d
Nikita <vip.nikita.2004@list.ru>
2021-02-22 13:44:43 +0500

    edit 1

--- js/script.js
+++ js/script.js
@@ -12,14 +12,3 @@ for (let anchor of anchors) {
     })
   })
 }
-
-wow = new WOW(
-  {
-  boxClass:     'wow',      // default
-  animateClass: 'animate__animated', // default
-  offset:       0,          // default
-  mobile:       true,       // default
-  live:         true        // default
-}
-)
-wow.init();
